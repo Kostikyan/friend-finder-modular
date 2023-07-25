8a29d94db72cb8de32d0f2758440d35ec59182ad
Vahe <120592399+Kostikyan@users.noreply.github.com>
2023-07-25 13:43:16 +0400

    Initial commit

---
+++ friend-finder-rest/src/main/java/com/friendfinder/friendfinderrest/FriendFinderRestApplication.java
@@ -0,0 +1,13 @@
+package com.friendfinder.friendfinderrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendFinderRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendFinderRestApplication.class, args);
+    }
+
+}
---
+++ friend-finder-web/src/main/java/com/friendfinder/friendfinderweb/FriendFinderWebApplication.java
@@ -0,0 +1,13 @@
+package com.friendfinder.friendfinderweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendFinderWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendFinderWebApplication.class, args);
+    }
+
+}
