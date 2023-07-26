23e62a753543c21f49ad50f7d62a14f84e70cdb4
Vahe <120592399+Kostikyan@users.noreply.github.com>
2023-07-26 18:58:47 +0400

    admin functions

--- friend-finder-common/src/main/java/com/friendfinder/friendfindercommon/mapper/CommentMapper.java
+++ friend-finder-common/src/main/java/com/friendfinder/friendfindercommon/mapper/CommentMapper.java
@@ -1,11 +1,8 @@
 package com.friendfinder.friendfindercommon.mapper;
 
 import com.friendfinder.friendfindercommon.dto.commentDto.CommentRequestDto;
-import com.friendfinder.friendfindercommon.dto.commentDto.GetCommentRequestDto;
 import com.friendfinder.friendfindercommon.entity.Comment;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
