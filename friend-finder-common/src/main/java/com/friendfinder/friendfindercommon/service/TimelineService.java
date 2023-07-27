c5e7cc3b46b3997b32d6ef16d5c712be6b5f2688
Vahe <120592399+Kostikyan@users.noreply.github.com>
2023-07-27 12:29:55 +0400

    commit and push updates

--- friend-finder-common/src/main/java/com/friendfinder/friendfindercommon/service/TimelineService.java
+++ friend-finder-common/src/main/java/com/friendfinder/friendfindercommon/service/TimelineService.java
@@ -1,11 +1,10 @@
 package com.friendfinder.friendfindercommon.service;
 
+import com.friendfinder.friendfindercommon.dto.userDto.UserUpdateRequestDto;
 import com.friendfinder.friendfindercommon.entity.Country;
 import com.friendfinder.friendfindercommon.entity.User;
 import com.friendfinder.friendfindercommon.entity.UserImage;
 import com.friendfinder.friendfindercommon.security.CurrentUser;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -13,9 +12,9 @@ import java.util.List;
 public interface TimelineService {
     List<Country> findAllCountries();
 
-    void updateUser(@ModelAttribute User user, @AuthenticationPrincipal CurrentUser currentUser);
+    User updateUser(UserUpdateRequestDto user, CurrentUser currentUser);
 
-    void updateUserProfilePic(MultipartFile profilePic, CurrentUser currentUser, UserImage userImage);
+    User updateUserProfilePic(MultipartFile profilePic, CurrentUser currentUser, UserImage userImage);
 
-    void updateUserProfileBackgroundPic(MultipartFile bgPic, CurrentUser currentUser);
+    User updateUserProfileBackgroundPic(MultipartFile bgPic, CurrentUser currentUser);
 }
--- friend-finder-web/src/main/java/com/friendfinder/friendfinderweb/controller/TimelineController.java
+++ friend-finder-web/src/main/java/com/friendfinder/friendfinderweb/controller/TimelineController.java
@@ -1,5 +1,6 @@
 package com.friendfinder.friendfinderweb.controller;
 
+import com.friendfinder.friendfindercommon.dto.userDto.UserUpdateRequestDto;
 import com.friendfinder.friendfindercommon.entity.User;
 import com.friendfinder.friendfindercommon.entity.UserImage;
 import com.friendfinder.friendfindercommon.security.CurrentUser;
@@ -40,14 +41,15 @@ public class TimelineController {
     }
 
     @PostMapping("/edit-basic")
-    public String editBasic(@ModelAttribute User user, @AuthenticationPrincipal CurrentUser currentUser) {
+    public String editBasic(@ModelAttribute UserUpdateRequestDto user, @AuthenticationPrincipal CurrentUser currentUser) {
         timelineService.updateUser(user, currentUser);
         return "redirect:/timeline/edit-basic";
     }
 
     @PostMapping("/change-profile-pic")
     public String changeProfilePic(@RequestParam("profile-pic") MultipartFile profilePic,
-                                   @AuthenticationPrincipal CurrentUser currentUser, @ModelAttribute UserImage userImage) {
+                                   @AuthenticationPrincipal CurrentUser currentUser,
+                                   @ModelAttribute UserImage userImage) {
         timelineService.updateUserProfilePic(profilePic, currentUser, userImage);
         return "redirect:/users/profile/" + currentUser.getUser().getId();
     }
